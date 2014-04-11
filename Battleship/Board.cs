cdd4a740b22ace6f56248f4d77e1cfd2d4ec0285
unknown <tuc51727@cis-wcc20915.tu.temple.edu>
2014-04-11 12:34:40 -0400

    Initial Commit

---
+++ Battleship/Board.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Board : IBoard
+    {
+
+        private IShip[] ships;
+        private Cell[,] cells;
+        //private Cell[][] cells = new Cell[10]; //C#
+        //public Cell[][] cells = new Cell[10][10]; //java
+
+        public int Rows
+        {
+            get { return 10; }
+        }
+
+        public int Columns
+        {
+            get { return 10; }
+        }
+
+        public IShip[] ShipsLeftToPlace
+        {
+            get { return ships; }
+        }
+
+        public bool Place(IShip ship, int row, int column, Orientation orientation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Fire(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cell[] Cells
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void NewGame()
+        {
+            ships = new IShip[] {
+                new Ship() { Size = 5},
+                new Ship() { Size = 4},
+                new Ship() { Size = 3},
+                new Ship() { Size = 3},
+                new Ship() { Size = 2}
+            };
+            cells = new Cell[10, 10];
+        }
+
+        public Cell[] Test
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Battleship/IGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    enum Player
+    {
+        Human,
+        Computer
+    }
+
+    interface IGame
+    {
+        //get 2 boards
+        IBoard[] Boards { get; }
+        //get whose turn it is
+        Player Turn { get; }
+        //fire on the active board
+        bool Fire(int row, int column);
+        //restarts the boards
+        void NewGame();
+    }
+}
