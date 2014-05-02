aa691521120240c4aebb8a6b86a96c7fab21828f
Logan Murphy <tuc51727@CIS-WCC20902.tu.temple.edu>
2014-05-02 13:15:10 -0400

    can now get information from the database

--- bs/Database.cs
+++ bs/Database.cs
@@ -12,6 +12,56 @@ namespace bs
 {
     public class Database
     {
+        public static Dictionary<string, Dictionary<string, object>> GetStatistics()
+        {
+            Dictionary<string, Dictionary<string, object>> list = new Dictionary<string, Dictionary<string, object>>();
+
+            string strConnection = "provider=Microsoft.ACE.OLEDB.12.0;Data Source=Battleship.accdb;";
+            string strSQL = "";
+            OleDbConnection myConnection = new OleDbConnection(strConnection);
+            OleDbCommand myCommand = new OleDbCommand(strSQL, myConnection);
+            try
+            {
+                myConnection.Open();
+                strSQL = "SELECT Name, Min(Turns), Count(*) FROM Statistic WHERE IsWinner GROUP BY Name ORDER BY Name";
+                myCommand.CommandText = strSQL;
+                myCommand.CommandType = System.Data.CommandType.Text;
+                OleDbDataReader reader = myCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Dictionary<string, object> fields = new Dictionary<string, object>();
+                    fields["Turns"] = reader.GetInt32(1).ToString();
+                    fields["Wins"] = reader.GetInt32(2).ToString();
+                    fields["Loses"] = 0;
+                    list[reader.GetString(0)] = fields;
+                }
+                reader.Close();
+
+                strSQL = "SELECT Name, Count(*) FROM Statistic WHERE NOT IsWinner GROUP BY Name ORDER BY Name";
+                myCommand.CommandText = strSQL;
+                myCommand.CommandType = System.Data.CommandType.Text;
+                reader = myCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (!list.ContainsKey(reader.GetString(0)))
+                    {
+                        Dictionary<string, object> fields = new Dictionary<string, object>();
+                        fields["Turns"] = 100;
+                        fields["Wins"] = 0;
+                        list[reader.GetString(0)] = fields;
+                    }
+                    list[reader.GetString(0)]["Loses"] = reader.GetInt32(1).ToString();
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            myConnection.Close();
+            return list;
+        }
+
         public static bool AddStatistics(String name, Player winner, int turns)
         {
             string strConnection = "provider=Microsoft.ACE.OLEDB.12.0;Data Source=Battleship.accdb;";
